933f6d6a708c830b62fc30515dd5e744dc4bf3ea
Isaias <Isaias.Mayon@noventiq.com>
2023-03-08 18:44:44 -0500

    chore: validaciones con atributos

--- NET_WebApp_Backend/Models/ProductMultimedia.cs
+++ NET_WebApp_Backend/Models/ProductMultimedia.cs
@@ -1,11 +1,17 @@
-using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.ComponentModel.DataAnnotations;
 
 namespace NET_WebApp_Backend.Models
 {
     public class ProductMultimedia
     {
+        [Required]
+        [Range(0,100)]
         public int Id { get; set; }
+        
+        [Required]
         public string Title { get; set; }
+        
+        [Required]
         public List<IFormFile> Images { get; set; }
     }
 }
