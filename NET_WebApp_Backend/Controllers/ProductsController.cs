7def5b83996a3c51ab4262f5e7da199edd1ee2e5
Isaias <Isaias.Mayon@noventiq.com>
2023-03-10 16:13:43 -0500

    chore: validación y respuesta avanzada

--- NET_WebApp_Backend/Controllers/ProductsController.cs
+++ NET_WebApp_Backend/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using NET_WebApp_Backend.Models;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Net;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace NET_WebApp_Backend.Controllers
@@ -49,12 +51,24 @@ namespace NET_WebApp_Backend.Controllers
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult> Create([FromBody] Product product)
+        public async Task<ActionResult> Create([FromBody] RequestProduct product)
         {
 
-            ProductValidator validator = new ProductValidator();
+            RequestProductValidator validator = new RequestProductValidator();
 
             ValidationResult result = validator.Validate(product);
+            if (!result.IsValid)
+            {
+                StatusResponseSimple respuesta = new StatusResponseSimple(false, "Revise los datos ingresado e intente de nuevo");
+
+                this._logger.LogError("{0}, {1}", respuesta.TraceId, JsonSerializer.Serialize(product));
+
+                respuesta.Success = false;
+                respuesta.Title = "Revise los datos ingresado e intente de nuevo";
+                respuesta.Errors = FluentValidationExtend.GetErrors(result.Errors);
+                return StatusCode(StatusCodes.Status400BadRequest, respuesta);
+            }
+
             if (!result.IsValid)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, null);
@@ -108,6 +122,8 @@ namespace NET_WebApp_Backend.Controllers
         [Route("{id}/fotos")]
         public async Task<ActionResult> UploadPhoto([FromRoute] int id, IList<IFormFile> files)
         {
+            StatusResponseSimple respuesta = new StatusResponseSimple(true, "");
+
             string uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
             foreach (IFormFile file in files)
             {
@@ -116,7 +132,20 @@ namespace NET_WebApp_Backend.Controllers
                     string filePath = Path.Combine(uploads, file.FileName);
                     using (Stream fileStream = new FileStream(filePath, FileMode.Create))
                     {
-                        await file.CopyToAsync(fileStream);
+                        try
+                        {
+                            throw new Exception("Error generado intencionalmente");
+                            await file.CopyToAsync(fileStream);
+                        }
+                        catch (Exception ex)
+                        {
+                            this._logger.LogError(ex, "No se pudo guardar el archivo {0}. Id : {1}", file.FileName, respuesta.TraceId);
+
+                            respuesta.Success = false;
+                            respuesta.Title = string.Format("No se pudo guardar el archivo {0}", file.FileName); ;
+                            respuesta.Detail = ex.ToString();
+                            return StatusCode(StatusCodes.Status500InternalServerError, respuesta);
+                        }
                     }
                 }
             }
---
+++ NET_WebApp_Backend/Models/StatusResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_WebApp_Backend.Models
+{
+    public class StatusResponse<T> : StatusResponseSimple
+    {
+        public T Data { get; set; }
+        public StatusResponse(bool success, string title) : base(success, title)
+        {
+        }
+    }
+}
