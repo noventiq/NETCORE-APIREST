4c4c1dd1d5b6f665fe151484c65f2ed6b079066e
Isaias Mayon <Isaias.Mayon@ibit.pe>
2023-03-15 17:23:59 -0500

    chore: script sql de usuarios y perfiles

--- NETCORE.Infraestructure/Sql/01.schema.sql
+++ NETCORE.Infraestructure/Sql/01.schema.sql
@@ -23,4 +23,46 @@ CREATE TABLE ProductThumbnails(
  createdBy varchar(50),
  updatedAt datetime,
  updatedBy varchar(50)
-)
+);
+
+CREATE TABLE carts (
+    [id] INT,
+    [products_id] INT,
+    [products_title] NVARCHAR(45),
+    [products_price] INT,
+    [products_quantity] INT,
+    [products_total] INT,
+    [products_discountPercentage] NUMERIC(4, 2),
+    [products_discountedPrice] INT,
+    [total] INT,
+    [discountedTotal] INT,
+    [userId] INT,
+    [totalProducts] INT,
+    [totalQuantity] INT
+);
+
+create table users(
+id INT PRIMARY KEY,
+username varchar(100),
+password varchar(100),
+email varchar(100)
+);
+
+create table profiles(
+id INT primary key,
+name varchar(100)
+);
+
+create table users_profiles (
+user_id INT,
+profile_id INT
+PRIMARY KEY (user_id,profile_id),
+CONSTRAINT FK_users_profiles_user_id FOREIGN KEY (user_id)
+        REFERENCES dbo.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+CONSTRAINT FK_users_profiles_profile_id FOREIGN KEY (profile_id)
+        REFERENCES dbo.profiles (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
--- NETCORE.Infraestructure/Sql/02.data.sql
+++ NETCORE.Infraestructure/Sql/02.data.sql
@@ -32,3 +32,145 @@ INSERT INTO Products (id, title, description, price, discountPercentage, rating,
 INSERT INTO Products (id, title, description, price, discountPercentage, rating, stock, brand, category) VALUES ('30', 'Key Holder', 'Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality', '30', '2.92', '4.92', '54', 'Golden', 'home-decoration');
 
 SET IDENTITY_INSERT Products OFF
+
+
+INSERT INTO carts 
+VALUES 
+(1,59,N'Spring and summershoes',20,3,60,8.71,55,NULL,NULL,NULL,NULL,NULL),
+(1,88,N'TC Reusable Silicone Magic Washing Gloves',29,2,58,3.19,56,NULL,NULL,NULL,NULL,NULL),
+(1,18,N'Oil Free Moisturizer 100ml',40,2,80,13.1,70,NULL,NULL,NULL,NULL,NULL),
+(1,95,N'Wholesale cargo lashing Belt',930,1,930,17.67,766,NULL,NULL,NULL,NULL,NULL),
+(1,39,N'Women Sweaters Wool',600,2,1200,17.2,994,NULL,NULL,NULL,NULL,NULL),
+	
+(2,96,N'lighting ceiling kitchen',30,2,60,14.89,51,2328,1941,97,5,10),
+(2,91,N'Black Motorbike',569,3,1707,13.63,1474,2328,1941,97,5,10),
+(2,9,N'Infinix INBOOK',1099,1,1099,11.83,969,2328,1941,97,5,10),
+(2,16,N'Hyaluronic Acid Serum',19,1,19,13.31,16,2328,1941,97,5,10),
+(2,54,N'Pubg Printed Graphic T-Shirt',46,3,138,16.44,115,2328,1941,97,5,10),
+
+(3,37,N'ank Tops for Womens/Girls',50,2,100,12.05,88,3023,2625,30,5,10),
+(3,80,N'Chain Pin Tassel Earrings',45,3,135,17.75,111,3023,2625,30,5,10),
+(3,68,N'Stylish Luxury Digital Watch',57,3,171,9.03,156,3023,2625,30,5,10),
+(3,81,N'Round Silver Frame Sun Glasses',19,1,19,10.1,17,3023,2625,30,5,10),
+(3,90,N'Cycle Bike Glow',35,1,35,11.08,31,3023,2625,30,5,10),
+
+(4,36,N'Sleeve Shirt Womens',90,1,90,10.89,80,460,403,63,5,10),
+(4,54,N'Pubg Printed Graphic T-Shirt',46,1,46,16.44,38,460,403,63,5,10),
+(4,11,N'perfume Oil',13,3,39,8.4,36,460,403,63,5,10),
+(4,47,N'Sneaker shoes',120,2,240,10.37,215,460,403,63,5,10),
+(4,64,N'Leather Strap Skeleton Watch',46,3,138,10.2,124,460,403,63,5,10),
+
+(5,23,N'Orange Essence Food Flavou',14,3,42,8.04,39,553,493,83,5,10),
+(5,91,N'Black Motorbike',569,1,569,13.63,491,553,493,83,5,10),
+(5,45,N'Malai Maxi Dress',50,2,100,5.07,95,553,493,83,5,10),
+(5,84,N'Square Sunglasses',28,1,28,13.89,24,553,493,83,5,10),
+(5,70,N'Stainless Steel Women',35,3,105,8.98,96,553,493,83,5,10),
+
+(6,53,N'printed high quality T shirts',35,3,105,7.54,97,844,745,58,5,10),
+(6,61,N'Leather Straps Wristwatch',120,2,240,7.14,223,844,745,58,5,10),
+(6,92,N'HOT SALE IN EUROPE electric racing motorcycle',920,1,920,14.4,788,844,745,58,5,10),
+(6,11,N'perfume Oil',13,3,39,8.4,36,844,745,58,5,10),
+(6,37,N'ank Tops for Womens/Girls',50,3,150,12.05,132,844,745,58,5,10),
+
+(7,61,N'Leather Straps Wristwatch',120,1,120,7.14,111,1454,1276,26,5,12),
+(7,80,N'Chain Pin Tassel Earrings',45,2,90,17.75,74,1454,1276,26,5,12),
+(7,99,N'American Vintage Wood Pendant Light',46,3,138,8.84,126,1454,1276,26,5,12),
+(7,14,N'Non-Alcoholic Concentrated Perfume Oil',120,1,120,15.6,101,1454,1276,26,5,12),
+(7,48,N'Women Strip Heel',40,3,120,10.83,107,1454,1276,26,5,12),
+
+(8,45,N'Malai Maxi Dress',50,1,50,5.07,47,588,519,56,5,10),
+(8,83,N'Wiley X Night Vision Yellow Glasses',30,3,90,6.33,84,588,519,56,5,10),
+(8,96,N'lighting ceiling kitchen',30,1,30,14.89,26,588,519,56,5,10),
+(8,21,N'- Daal Masoor 500 grams',20,3,60,4.81,57,588,519,56,5,10),
+(8,2,N'iphone X',899,1,899,17.94,738,588,519,56,5,10),
+
+(9,74,N'Leather Hand Bag',57,3,171,11.19,152,1129,952,1,5,9),
+(9,10,N'HP Pavilion 15-DK1056WM',1099,3,3297,6.18,3093,1129,952,1,5,9),
+(9,19,N'Skin Beauty Serum.',46,2,92,10.68,82,1129,952,1,5,9),
+(9,53,N'printed high quality T shirts',35,1,35,7.54,32,1129,952,1,5,9),
+(9,13,N'Fog Scent Xpressio Perfume',13,1,13,8.14,12,1129,952,1,5,9),
+
+(10,75,N'Seven Pocket Women Bag',68,1,68,14.87,58,3608,3371,91,5,10),
+(10,39,N'Women Sweaters Wool',600,1,600,17.2,497,3608,3371,91,5,10),
+(10,3,N'Samsung Universe 9',1249,3,3747,15.46,3168,3608,3371,91,5,10),
+(10,7,N'Samsung Galaxy Book',1499,1,1499,4.15,1437,3608,3371,91,5,10),
+(10,93,N'Automatic Motor Gas Motorcycles',1050,3,3150,3.34,3045,3608,3371,91,5,10),
+
+(11,71,N'Women Shoulder Bags',46,3,138,14.65,118,9064,8205,13,5,9),
+(11,25,N'Gulab Powder 50 Gram',70,2,140,13.58,121,9064,8205,13,5,9),
+(11,65,N'Stainless Steel Wrist Watch',47,3,141,17.79,116,9064,8205,13,5,9),
+(11,58,N'formal offices shoes',57,1,57,12,50,9064,8205,13,5,9),
+(11,53,N'printed high quality T shirts',35,3,105,7.54,97,9064,8205,13,5,9),
+
+(12,32,N'Sofa for Coffe Cafe',50,2,100,15.59,84,581,502,66,5,12),
+(12,41,N'NIGHT SUIT',55,3,165,15.05,140,581,502,66,5,12),
+(12,69,N'Golden Watch Pearls Bracelet Watch',47,1,47,17.55,39,581,502,66,5,12),
+(12,98,N'3 lights lndenpant kitchen islang',34,3,102,5.92,96,581,502,66,5,12),
+(12,67,N'Fashion Magnetic Wrist Watch',60,2,120,16.69,100,581,502,66,5,12),
+
+(13,81,N'Round Silver Frame Sun Glasses',19,1,19,10.1,17,534,459,52,5,11),
+(13,42,N'Stiched Kurta plus trouser',80,2,160,15.37,135,534,459,52,5,11),
+(13,29,N'Handcraft Chinese style',60,3,180,15.34,152,534,459,52,5,11),
+(13,64,N'Leather Strap Skeleton Watch',46,2,92,10.2,83,534,459,52,5,11),
+(13,54,N'Pubg Printed Graphic T-Shirt',46,1,46,16.44,38,534,459,52,5,11),
+
+(14,64,N'Leather Strap Skeleton Watch',46,2,92,10.2,83,497,425,79,5,9),
+(14,76,N'Silver Ring Set Women',70,2,140,13.57,121,497,425,79,5,9),
+(14,30,N'Key Holder',30,2,60,2.92,58,497,425,79,5,9),
+(14,88,N'TC Reusable Silicone Magic Washing Gloves',29,1,29,3.19,28,497,425,79,5,9),
+(14,94,N'new arrivals Fashion motocross goggles',900,2,1800,3.85,1731,497,425,79,5,9),
+
+(15,4,N'OPPOF19',280,1,280,17.91,230,2121,2021,76,5,9),
+(15,100,N'Crystal chandelier maria theresa for 12 light',47,3,141,16,118,2121,2021,76,5,9),
+(15,1,N'iPhone 9',549,2,1098,12.96,956,2121,2021,76,5,9),
+(15,48,N'Women Strip Heel',40,3,120,10.83,107,2121,2021,76,5,9),
+(15,94,N'new arrivals Fashion motocross goggles',900,3,2700,3.85,2596,2121,2021,76,5,9),
+
+(16,3,N'Samsung Universe 9',1249,3,3747,15.46,3168,4339,4007,47,5,12),
+(16,50,N'Women Shoes',36,3,108,16.87,90,4339,4007,47,5,12),
+(16,67,N'Fashion Magnetic Wrist Watch',60,2,120,16.69,100,4339,4007,47,5,12),
+(16,86,N'Bluetooth Aux',25,1,25,10.56,22,4339,4007,47,5,12),
+(16,12,N'Brown Perfume',40,1,40,15.66,34,4339,4007,47,5,12),
+
+(17,12,N'Brown Perfume',40,3,120,15.66,101,4040,3414,15,5,10),
+(17,25,N'Gulab Powder 50 Gram',70,1,70,13.58,60,4040,3414,15,5,10),
+(17,24,N'cereals muesli fruit nuts',46,2,92,16.8,77,4040,3414,15,5,10),
+(17,90,N'Cycle Bike Glow',35,1,35,11.08,31,4040,3414,15,5,10),
+(17,97,N'Metal Ceramic Flower',35,1,35,10.94,31,4040,3414,15,5,10),
+
+(18,23,N'Orange Essence Food Flavou',14,2,28,8.04,26,352,300,56,5,8),
+(18,25,N'Gulab Powder 50 Gram',70,2,140,13.58,121,352,300,56,5,8),
+(18,9,N'Infinix INBOOK',1099,2,2198,11.83,1938,352,300,56,5,8),
+(18,78,N'Rhinestone Korean Style Open Rings',30,2,60,8.02,55,352,300,56,5,8),
+(18,63,N'Royal Blue Premium Watch',50,1,50,2.56,49,352,300,56,5,8),
+
+(19,43,N'frock gold printed',600,3,1800,15.55,1520,2476,2189,42,5,9),
+(19,77,N'Rose Ring',100,3,300,3.22,290,2476,2189,42,5,9),
+(19,50,N'Women Shoes',36,3,108,16.87,90,2476,2189,42,5,9),
+(19,31,N'Mornadi Velvet Bed',40,2,80,17,66,2476,2189,42,5,9),
+(19,75,N'Seven Pocket Women Bag',68,3,204,14.87,174,2476,2189,42,5,9),
+
+(20,66,N'Steel Analog Couple Watches',35,3,105,3.23,102,2492,2140,5,5,14),
+(20,59,N'Spring and summershoes',20,1,20,8.71,18,2492,2140,5,5,14),
+(20,29,N'Handcraft Chinese style',60,1,60,15.34,51,2492,2140,5,5,14),
+(20,32,N'Sofa for Coffe Cafe',50,1,50,15.59,42,2492,2140,5,5,14),
+(20,46,N'women''s shoes',40,2,80,16.96,66,2492,2140,5,5,14);
+
+
+insert into users(id,username,password,email)
+VALUES
+(1,'admin','admin','admin@empresa.pe'),
+(2,'invitado1','invitado1','invitado1@empresa.pe'),
+(3,'invitado2','invitado2','invitado2@empresa.pe');
+
+insert into profiles(id, name)
+values
+(1, 'admin'),
+(2, 'invitado');
+
+
+insert into users_profiles(user_id, profile_id)
+values
+(1,1),
+(2,2),
+(3,2);
