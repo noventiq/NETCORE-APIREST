db11e7d88101f4893cb64eaf6fd01b250bdde15c
Isaias Mayon <Isaias.Mayon@ibit.pe>
2023-03-17 20:36:01 -0500

    chore: datos aleatorios de un item de un dashboard de tipo linea

--- NETCORE.API/Controllers/WebSocketsController.cs
+++ NETCORE.API/Controllers/WebSocketsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NETCORE.Domain.Dashboard.DTO;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 
 namespace NETCORE.API.Controllers
 {
@@ -16,49 +18,41 @@ namespace NETCORE.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet("/ws")]
-        public async Task Get()
-        {
-            if (HttpContext.WebSockets.IsWebSocketRequest)
-            {
-                //using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                //_logger.Log(LogLevel.Information, "WebSocket connection established");
-                //await Echo(webSocket);
-                using (var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync())
-                {
-                    while (true)
-                    {
-                        await webSocket.SendAsync(Encoding.ASCII.GetBytes("{ \"now\": \"" + DateTime.Now + "\" }"), WebSocketMessageType.Text, true, CancellationToken.None);
-                        await Task.Delay(1000);
-                    }
-                }
-            }
-            else
-            {
-                HttpContext.Response.StatusCode = 400;
-            }
-        }
+        //[HttpGet("/ws")]
+        //public async Task Get()
+        //{
+        //    if (!HttpContext.WebSockets.IsWebSocketRequest)
+        //    {
+        //        HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        //        return;
+        //    }
 
-        private async Task Echo(WebSocket webSocket)
-        {
-            var buffer = new byte[1024 * 4];
-            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            _logger.Log(LogLevel.Information, "Message received from Client");
+        //    using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+        //    _logger.Log(LogLevel.Information, "WebSocket connection established");
+        //    await Echo(webSocket);
 
-            while (!result.CloseStatus.HasValue)
-            {
-                var serverMsg = Encoding.UTF8.GetBytes($"Server: Hello. You said: {Encoding.UTF8.GetString(buffer)}");
-                await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
-                _logger.Log(LogLevel.Information, "Message sent to Client");
+        //}
 
-                buffer = new byte[1024 * 4];
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                _logger.Log(LogLevel.Information, "Message received from Client");
+        //private async Task Echo(WebSocket webSocket)
+        //{
+        //    var buffer = new byte[1024 * 4];
+        //    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //    _logger.Log(LogLevel.Information, "Message received from Client");
 
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-            _logger.Log(LogLevel.Information, "WebSocket connection closed");
-        }
+        //    while (!result.CloseStatus.HasValue)
+        //    {
+        //        var serverMsg = Encoding.UTF8.GetBytes($"Server: Hello. You said: {Encoding.UTF8.GetString(buffer)}");
+        //        await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+        //        _logger.Log(LogLevel.Information, "Message sent to Client");
+
+        //        buffer = new byte[1024 * 4];
+        //        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //        _logger.Log(LogLevel.Information, "Message received from Client");
+
+        //    }
+        //    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        //    _logger.Log(LogLevel.Information, "WebSocket connection closed");
+        //}
 
         [HttpGet("/time")]
         public async Task GetTime()
@@ -90,7 +84,63 @@ namespace NETCORE.API.Controllers
 
                     while (isConnectionOpen)
                     {
-                        await webSocket.SendAsync(Encoding.ASCII.GetBytes("{ \"now\": \"" + DateTime.Now + "\" }"), WebSocketMessageType.Text, true, CancellationToken.None);
+                        await webSocket.SendAsync(Encoding.ASCII.GetBytes("{ \"source\": \"Server\", \"content\": \"" + DateTime.Now + "\" }"), WebSocketMessageType.Text, true, CancellationToken.None);
+                        await Task.Delay(1000);
+                    }
+
+                    if (!isConnectionOpen)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.Empty, "Cerrado", CancellationToken.None);
+                    }
+                }
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        [HttpGet("/dashboard")]
+        public async Task GetDashboard()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using (var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync())
+                {
+                    bool isConnectionOpen = true;
+                    var buffer = new byte[1024 * 4];
+                    Task.Run(async () =>
+                    {
+                        WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                        while (!result.CloseStatus.HasValue)
+                        {
+                            //string message = Encoding.UTF8.GetString(buffer).Replace("\0", "");
+                            //var serverMsg = Encoding.UTF8.GetBytes(message);
+
+                            //await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                        }
+
+                        if (result.CloseStatus.HasValue)
+                            isConnectionOpen = false;
+
+                    });
+
+                    while (isConnectionOpen)
+                    {
+                        int range = 100;
+                        Random rSale = new Random();
+                        Random rProfit = new Random();
+
+                        ResponseLinechartItem item = new ResponseLinechartItem();
+                        item.Label = DateTime.Now.ToString("yyyy-MM-dd");
+                        item.Sale = rSale.NextDouble() * range;
+                        item.Profit = rProfit.NextDouble() * range;
+                        string msg = JsonSerializer.Serialize(item);
+
+                        await webSocket.SendAsync(Encoding.ASCII.GetBytes(msg), WebSocketMessageType.Text, true, CancellationToken.None);
                         await Task.Delay(1000);
                     }
 
---
+++ NETCORE.Domain/Dashboard/DTO/ResponseLinechartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETCORE.Domain.Dashboard.DTO
+{
+    public class ResponseLinechartItem
+    {
+        public string Label { get; set; }
+        public double Sale { get; set; }
+        public double Profit { get; set; }
+    }
+}
